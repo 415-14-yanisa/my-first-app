057e30b4792d82f63c549b966c61299f02abd3e3
415-14-yanisa <premeyanisa@gmail.com>
2026-08-06 10:29:42 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 #ส่วนที่ 1 หัวข้อหน้าเว็บ(Title สีแดง)
-st.markdown("# :red[🧾 คำนวณค่าดัชนีมวลกาย BMI]")
+st.markdown("# :red[🧾 คำนวณค่าดัชนีมวลกาย BMI")
 st.write("45 กิโลกรัม 162 เซนติเมตร")
 
 #ส่วนที่ 2 สร้างช่องรับค่าน้ำหนัก และ ส่วนสูง
@@ -11,13 +11,13 @@ height_cm = st.number_input("162 (เซนติเมตร):",min_value=1.0)
 #ส่วนที่ 3 สร้างปุ่มกดคำนวณ
 if st.button("คำนวณ BMI 📝 "):
   # แปลงส่วนสูง จาก cm เป็น เมตร แล้วคำนวณ BMI
-  height_m = height_cm / 100
-  bmi = weight / (height_m ** 2)
+    height_m = height_cm / 100
+    bmi = weight / (height_m ** 2)
 
-st.write("---")
-st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+  st.write("---")
+  st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
 
-#ส่วนที่ 4 แปลผลค่า BMI ตามเเกณฑ์
+  #ส่วนที่ 4 แปลผลค่า BMI ตามเเกณฑ์
   if bmi < 18.5:
       st.warning("⚠️ คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
   elif 18.5 <= bmi < 23.0:
