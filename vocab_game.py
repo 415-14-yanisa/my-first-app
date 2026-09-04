323fc8a96eb0edc1ad05e35f6c95adba6edd663a
415-14-yanisa <premeyanisa@gmail.com>
2026-09-04 13:45:03 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -102,5 +102,5 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวญาณิศา ทอสาร เลขที่ 14 ม.4/15")
 
